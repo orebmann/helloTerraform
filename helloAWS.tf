c8c23b524b0a769ae2ee9e16d49d9f2bfa9d234c
Oliver Rebmann <orebm42@gmail.com>
2021-01-31 16:13:40 +0100

    Update helloAWS.tf

--- helloAWS.tf
+++ helloAWS.tf
@@ -7,6 +7,17 @@ terraform {
   }
 }
 
+  
+variable aws_access_key {
+  description = "The AWS API key needed to deploy resources"
+  type        = string
+}
+
+variable aws_secret_key {
+  description = "The AWS API key needed to deploy resources"
+  type        = string
+}
+
 provider "aws" {
   profile = "default"
   region  = "us-west-2"
