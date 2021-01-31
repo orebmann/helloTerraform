25188fcd6ec777c75194f474535c404dc14c6ce7
Oliver Rebmann <orebm42@gmail.com>
2021-01-31 16:14:15 +0100

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable aws_access_key {
-  description = "The AWS API key needed to deploy resources"
-  type        = string
-}
-
-variable aws_secret_key {
-  description = "The AWS API key needed to deploy resources"
-  type        = string
-}
