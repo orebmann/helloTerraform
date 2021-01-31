8d0ac533f22f075edb2852840c944b0b66e7d083
Oliver Rebmann <orebm42@gmail.com>
2021-01-31 16:09:27 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,9 @@
-variable aws_api_key {
+variable aws_access_key {
+  description = "The AWS API key needed to deploy resources"
+  type        = string
+}
+
+variable aws_secret_key {
   description = "The AWS API key needed to deploy resources"
   type        = string
 }
