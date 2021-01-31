c6281c5aa5eecf42ee62a14256fdf193d11a4c32
Oliver Rebmann <orebm42@gmail.com>
2021-01-31 16:14:04 +0100

    Update helloAWS.tf

--- helloAWS.tf
+++ helloAWS.tf
@@ -10,12 +10,10 @@ terraform {
   
 variable aws_access_key {
   description = "The AWS API key needed to deploy resources"
-  type        = string
 }
 
 variable aws_secret_key {
   description = "The AWS API key needed to deploy resources"
-  type        = string
 }
 
 provider "aws" {
